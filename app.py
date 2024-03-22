fe2411a4c093cb567b6b40d1b3797082a8c76c2d
abkazan <abkazan@wisc.edu>
2024-03-22 12:03:22 -0500

    added root page

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World I am groot!'
+def root_page():
+	# Sample data to pass to the template
+	title = "My Server"
+	message = "Mazel Tov, you made it to the server!!!"
+	subheading = "There's not really much to see here, so feel free to carry on. Thanks for stopping by and have a nice day!"
+	link = giannis_link()
+	return render_template("index.html", title=title, message=message, subheading=subheading, link=link)
