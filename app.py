8efd52140fef52ec5962d1fe291417f17ca23ab9
abkazan <abkazan@wisc.edu>
2024-03-22 12:38:20 -0500

    added testing webhook and lookup class

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify, render_template, request
 from twilio.twiml.voice_response import VoiceResponse
 from hubspot_contacts import get_props_from_number
+from lookup import lookup
+import time
 app = Flask(__name__)
+
+
+
 
 
 @app.route('/')
@@ -29,6 +34,45 @@ def get_data():
 def giannis_link(): 
     return "https://en.wikipedia.org/wiki/Giannis_Antetokounmpo"
 
+def call_routing_logic(from_number):
+    try:
+        lookup_results = lookup(from_number).get_right_contact()
+        print(lookup_results)
+    except Exception as e:
+        print(f'error searching for {from_number}, returning default value')
+        return "414-336-7659"
+    if type(lookup_results) != dict:
+        print(f'contact not found or multiple contacts found for number {from_number}, returning default value')
+        return "414-336-7659"
+    if lookup_results["hs_lead_status"] == None:
+        print("no lead status found, returning default value")
+        return "414-336-7659"
+    lead_status = lookup_results["hs_lead_status"].lower()
+    case_type = lookup_results["case_type"]
+    if 'future pay' in lead_status or 'at consult' in lead_status or 'after consult' in lead_status:
+        print(f'contact found with lead status: {lead_status}')
+        return "414-316-3555"
+    elif 'active' in lead_status:
+        print("contact found with active case")
+        if case_type == "Chapter 13 Bankruptcy":
+            print("active case found with chapter 13 Bankruptcy")
+            return "414-395-4525"
+        elif case_type == "Chapter 7 Bankruptcy":
+            print("active case found with chapter 7 Bankruptcy")
+            return "414-395-4513"
+        elif case_type == "Chapter 128":
+            print("active case found with chapter 128 Bankruptcy")
+            return "414-395-4511"
+    elif 'bad timing' in lead_status or 'no hire' in lead_status:
+        print('contact found with no hire or rehired - no hire')
+        return "414-567-3205"
+    elif 'immigration' in lead_status:
+        print('immigration contact found')
+        return "414-567-3209"
+
+    return "414-336-7569"
+
+
 @app.route("/callRouter", methods=["POST"])
 def handle_call():
     #print(request.form)
@@ -46,3 +90,15 @@ def handle_call():
 def hubspot_lookup_test():
     ret = get_props_from_number("+14146072404")
     return jsonify(ret)
+
+@app.route("/testWebhook", methods=["POST"])
+def test_hook():
+    start = time.time()
+    data = request.get_json()
+    name = data['customer_name']
+    number = data['customer_phone_number']
+    print(f"call recieved from {name} at number {number} with the following routing logic results:")
+    print(f"Number would be forwarded to {call_routing_logic(number)}")
+    end = time.time()
+    print(f"operations completed in {end - start} seconds")
+    return "", 200
---
+++ lookup.py
@@ -0,0 +1,64 @@
+#first_format   +14146072404
+#second_format  414-607-2404
+#third_format   14146072404
+#fourth_format  +1 (414)607-2404
+
+from hubspot_contacts import get_props_from_number
+
+class lookup:
+    def __init__(self, from_number):
+        self.from_number = from_number
+        self.formats = {
+            "first": self.from_number,
+            "second": self.dash_format(),
+            "third": self.third_format(),
+            "fourth": self.fourth_format(),
+            "fifth": self.fifth_format(),
+        }
+        
+        
+    #go through lookup results and determine which one is right
+    #ideally, most should be none, but its not always consistent
+    def get_right_contact(self):
+        non_none = []
+        lookups = self.preform_lookups()
+        for result in lookups:
+            if lookups[result] is not None:
+                non_none.append(lookups[result])
+        if len(non_none) == 1:
+            return  non_none[0]
+        else:
+            return non_none
+            
+        
+    def preform_lookups(self):
+        #initalize the return object
+        results = {"first": None, "second": None, "third": None, "fourth": None, "fifth": None}
+        for key in self.formats:
+            #print(self.formats[key])
+            lookup_result = get_id_from_number(self.formats[key])
+            #print(lookup_result)
+            if lookup_result not in results.values():
+                results[key] = lookup_result
+        return results
+        
+    
+    def dash_format(self):
+        return f"{self.from_number[2:5]}-{self.from_number[5:8]}-{self.from_number[8:]}"
+    
+    def third_format(self):
+        return self.from_number[1:]
+    
+    def fourth_format(self):
+        return f"{self.from_number[0:2]} ({self.from_number[2:5]}){self.from_number[5:8]}-{self.from_number[8:]}"
+    
+    def fifth_format(self):
+        return f" {self.dash_format()}"
+    
+    def __str__(self):
+        ret = ""
+        for key in self.formats.keys():
+            ret += f"{key}: {self.formats[key]}\n"
+        return ret
+        
+
